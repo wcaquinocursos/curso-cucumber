02c40c07dd0ca58ea122b0939349edc7a8de536d
Wagner Aquino <wcaquino@gmail.com>
2020-03-16 11:08:40 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/runners/RunnerTest.java
@@ -0,0 +1,20 @@
+package br.ce.wcaquino.runners;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features/",
+		glue = {"br.ce.wcaquino.steps", "br.ce.wcaquino.config"},
+		tags = {"@unitários", "not @ignore"},
+		plugin = {"pretty", "html:target/report-html", "json:target/report.json"},
+		monochrome = false,
+		snippets = SnippetType.CAMELCASE,
+		dryRun = false,
+		strict = false
+		)
+public class RunnerTest {
+}
